1068f9800bacdcebac498a4d9a2cbdf6bb02fb90
auycro <auycro@users.noreply.github.com>
2017-09-15 14:41:32 +0900

    Add files via upload

---
+++ plotter.py
@@ -0,0 +1,11 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 5, 0.1);
+y = np.sin(x)
+plt.plot(x, y)
+#plt.show()
+plt.savefig('foo.png')
